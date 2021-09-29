847f89cf13d65d96def5bdd40e4077f242023f15
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2021-09-29 12:05:32 -0500

    🐜🚫 Fix css bug not taking full width

--- src/global.js
+++ src/global.js
@@ -60,8 +60,9 @@ export const default_services = [
 ];
 
 export const HEAD = '<head>' +
-  '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />\n' +
-  `<title>${chrome.i18n.getMessage('title')}</title>\n` +
+  '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />' +
+  '<meta name="viewport" content="width=device-width,initial-scale=1">' +
+  `<title>${chrome.i18n.getMessage('title')}</title>` +
   '<link href="global.css" rel="stylesheet" type="text/css" />' +
   '<script src="popup.js" type="module"></script>' +
   '</head>';
