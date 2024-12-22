dcffe7e7d65daeca0fb23eb6ad94674dea1ec40b
Ethan Dawes <ethand.python@gmail.com>
2024-12-22 12:15:38 -0500

    load accounts in order

--- src/onboarding/1.js
+++ src/onboarding/1.js
@@ -5,16 +5,19 @@ function autoConnect() {
 }
 
 function generateAccounts() {
-  chrome.storage.sync.get(['users'], storage => {
+  chrome.storage.sync.get(['users', 'userOrder'], storage => {
     const users = storage.users || [];
+    const userOrder = storage.userOrder || [];
     let accountsHtml = '';
-    for (const user of Object.values(users)) {
+    for (const userEmail of userOrder) {
+      const user = users[userEmail];
       accountsHtml +=
 `<div class="account" style="flex: 1; padding: 0.5rem 0;">
     <input class="checkbox-img" type="checkbox" name="${user.email}" title="${user.name}" style="background-image: url('${user.icon}');">
     <div>
         ${user.name}<br />
-        id: <input type="number" min="0" value="${user.ID}" />
+        id: <input type="number" min="0" data-email="${user.email}" value="${user.ID}" style="width: 3em" />
+        <button title="remove" data-email="${user.email}">🗑️</button>
     </div>
 </div>`;
     }
