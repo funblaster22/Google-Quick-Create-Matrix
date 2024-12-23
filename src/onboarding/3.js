b8693eb8fde64ec0a07dec8ff8e30399b5834a5f
Ethan Dawes <ethand.python@gmail.com>
2024-12-22 21:02:26 -0500

    bugfix last onboarding page

--- src/onboarding/3.js
+++ src/onboarding/3.js
@@ -1,6 +1,8 @@
 import generateTable from "../Table.js";
 import {localizeHtmlPage} from "../global.js";
+import {makeTablePreview} from "../options.js";
 
+makeTablePreview();
 localizeHtmlPage();
 
 generateTable(false, false).then(table => {
