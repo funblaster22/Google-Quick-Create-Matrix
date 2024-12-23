75aa122626602207422314f634f3d6517fb76f01
Ethan Dawes <ethand.python@gmail.com>
2024-12-23 01:22:44 -0500

    fix localization on first run popup

--- src/options.js
+++ src/options.js
@@ -19,7 +19,7 @@ export function makeTablePreview() {
     }
 
     generateTable(true).then(newTable => {
-      if (location.pathname === '/options.html') {
+      if (!location.pathname.startsWith('/onboarding')) {
         const existingTable = document.getElementsByClassName('grid-container')[0];
         existingTable?.replaceWith(newTable);
 
@@ -77,7 +77,7 @@ function updateSettings(ev) {
   });
 }
 
-if (location.pathname === '/options.html') {
+if (!location.pathname.startsWith('/onboarding')) {
   localizeHtmlPage();
   makeTablePreview();
 
